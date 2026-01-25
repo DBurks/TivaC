9e58df0b34a8012ff8bfaaeb01bb31be71873507
Damien Burks <damien.burks.1@gmail.com>
2026-01-25 09:21:28 -0700

    Milestone 2: Added Sinewave logicand ANSI Terminal Dashboard

--- TivaC_UART_Project/TivaC_UART_Project.c
+++ TivaC_UART_Project/TivaC_UART_Project.c
@@ -1,24 +1,20 @@
 //*****************************************************************************
 //
-// hello.c - Simple hello world example.
+// TivaC_UART_Project.c - A UART Project demonstration
 //
-// Copyright (c) 2012-2020 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.2.0.295 of the EK-TM4C123GXL Firmware Package.
+// Modifes a simple helo world application to demonstrate different UART 
+// formatting techniques.
+//
+// First we'll go through printing out a simulated temperature periodically 
+// to the Serial Console. This will produce an endless stream of text.
+//
+// NJesxt use ANSI formatting, to produce a dash board showing a header 
+// and the temperature. The temperature will be colored properly 
+// depending upon whether it is above the max, between max and min ro below
+// the min threshold value.
+//
+// Finally output json or csv over UART so the user can format the data,
+// perhaps using an external application/
 //
 //*****************************************************************************
 
@@ -35,14 +31,27 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
+#include <math.h>
+
+// ANSI Escpae Codes
+#define ANSI_COLOR_RED    "\x1b[31m"   // color if temp above max
+#define ANSI_COLOR_GREEN  "\x1b[32m"   // color if between max and min
+#define ANSI_COLOR_BLUE   "\x1b[34m"   // color if temp below min
+#define ANSI_COLOR_RESET  "\x1b[0m"    // reset color (black)
+#define ANSI_CLEAR_SCREEN "\x1b[2J"    // ansi control characters to clear screen
+#define ANSI_CURSOR_HOME  "\x1b[H"     // returns the cursor to the home position
+
+// Simulation Constraints
+#define TEMP_MIN_ALARM   20.0
+#define TEMP_MAX_ALARM   30.0
+#define TEMP_MID         25.0
+#define TEMP_AMP         10.0
+
 
 //*****************************************************************************
 //
 //! \addtogroup example_list
-//! <h1>Hello World (hello)</h1>
-//!
-//! A very simple ``hello world'' example.  It simply displays ``Hello World!''
-//! on the UART and is a starting point for more complicated applications.
+//! <h1> TivaC Series UART Demonstration</h1>
 //!
 //! UART0, connected to the Virtual Serial Port and running at
 //! 115,200, 8-N-1, is used to display messages from this application.
@@ -97,15 +106,31 @@ ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
+void
+DisplayTemp(float current_temp) {
+    
+    // compute the color to use
+    char* color = ANSI_COLOR_GREEN;
+    if (current_temp >= TEMP_MAX_ALARM) color = ANSI_COLOR_RED;
+    else if (current_temp <= TEMP_MIN_ALARM) color = ANSI_COLOR_BLUE;
+        
+    // use home to overwrite dashboard in place
+    UARTprintf(ANSI_CURSOR_HOME);
+    UARTprintf("----------------------------------\n");
+    UARTprintf("HTIVA C LIVE TELEMETRY            \n");
+    UARTprintf("----------------------------------\n");
+    UARTprintf(" Temp: %s%d.%d C%s \n", color, (int) current_temp, (int)(current_temp *10) %10, ANSI_COLOR_RESET);
+    UARTprintf("----------------------------------\n");
+}
+
 //*****************************************************************************
 //
-// Print "Hello World!" to the UART on the evaluation board.
+// Print text with the Temperature data to the UART
 //
 //*****************************************************************************
 int
 main(void)
 {
-    //volatile uint32_t ui32Loop;
 
     //
     // Enable lazy stacking for interrupt handlers.  This allows floating-point
@@ -121,48 +146,28 @@ main(void)
                        SYSCTL_OSC_MAIN);
 
     //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-
-    //
-    // Enable the GPIO pins for the LED (PF2 & PF3).
-    //
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-
-    //
     // Initialize the UART.
     //
     ConfigureUART();
 
-    //
-    // Hello!
-    //
-    UARTprintf("Hello, world!\n");
+    // we need an angle to feed into the sine functino
+    float angle = 0.0;
 
     //
     // We are finished.  Hang around doing nothing.
     //
     while(1)
     {
-        //
-        // Turn on the BLUE LED.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        // calculate our simulated temperature
+        float current_temp = TEMP_MID + TEMP_AMP * sinf(angle);
 
-        //
-        // Delay for a bit.
-        //
-        SysCtlDelay(SysCtlClockGet() / 10 / 3);
+        // display the temperature data
+        DisplayTemp(current_temp);
 
-        //
-        // Turn off the BLUE LED.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-
-        //
-        // Delay for a bit.
-        //
-        SysCtlDelay(SysCtlClockGet() / 10 / 3);
+        // change our angle evry cycle
+        angle += 0.05;
+        
+        // wait a little bit
+        SysCtlDelay(SysCtlClockGet() / 10);
     }
 }
